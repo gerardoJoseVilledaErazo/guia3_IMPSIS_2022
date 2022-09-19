94ae2ae7002d1b9cd4a503208549109fad8e058d
gerardoJoseVilledaErazo <gerardovilleda@gmail.com>
2022-09-18 20:46:20 -0600

    Se actualizaron varios archivos

--- application/views/plantillas/navbar.php
+++ application/views/plantillas/navbar.php
@@ -9,16 +9,16 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="../paginas/suma.php">Suma<span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="#">Suma<span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="aritmetica/view/resta">Resta</a>
+              <a class="nav-link" href="#">Resta</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="aritmetica/view/multiplicacion">Multiplicacion</a>
+              <a class="nav-link" href="#">Multiplicacion</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="aritmetica/view/division">Division</a>
+              <a class="nav-link" href="#">Division</a>
             </li>
           </ul>
             <form class="form-inline my-2 my-lg-0">

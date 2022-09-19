2a681d8d67e0cef63f8bd2d63144d7a293a700af
gerardoJoseVilledaErazo <gerardovilleda@gmail.com>
2022-09-18 19:29:43 -0600

    Se creo el archivo suma.php en la carpeta paginas

---
+++ application/views/paginas/suma.php
@@ -0,0 +1,6 @@
+<form action="../sumar" method="POST">
+    <p>Numero 1: <input type="number" name="num1"></p>
+    <p>Numero 2: <input type="number" name="num2"></p>
+    <p> &emsp; <input type="submit" value="Sumar"> <input type="reset" value="Limpiar"></p>
+    
+</form>

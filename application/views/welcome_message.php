5d1a18929e4398ab66570c2dfb891a5f0b0f81c5
gerardoJoseVilledaErazo <gerardovilleda@gmail.com>
2022-09-18 20:04:56 -0600

    Se actualizo el archivo welcome_message.php en la carpeta views

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -66,6 +66,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	</style>
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">USO</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="../views/paginas/suma.php">Suma<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../views/paginas/resta.php">Resta</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../views/paginas/multiplicacion.php">Multiplicacion</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../views/paginas/division.php">Division</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
 
 <div id="container">
 	<h1>Welcome to CodeIgniter!</h1>

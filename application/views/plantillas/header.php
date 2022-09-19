3c15a9e2f0dd9ed3a4b06168b042aa477c1bae7b
gerardoJoseVilledaErazo <gerardovilleda@gmail.com>
2022-09-18 19:24:20 -0600

    Se creo el archivo header.php en la carpeta plantillas

---
+++ application/views/plantillas/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initialscale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo $titulo ?></title>
+</head>
+<body>
+    <h1>Guía de Implantación 03 - 2022</h1>
+    <strong>Alumno: </strong> <p>Gerardo José Villeda Erazo</p> <br>
+    <strong>Código: </strong> <p>VE16-I04-001</p> <br>
+    <strong>Materia: </strong> <P>Implantación de Sistemas</P>  <br>

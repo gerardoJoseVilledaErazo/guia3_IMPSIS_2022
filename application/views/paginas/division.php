1c6e244aad8ffd48fdc21748c00d62c208f0f095
gerardoJoseVilledaErazo <gerardovilleda@gmail.com>
2022-09-18 20:00:02 -0600

    Se creo el archivo division.php en la carpeta paginas

---
+++ application/views/paginas/division.php
@@ -0,0 +1,7 @@
+<form action="../dividir" method="POST">
+    <p>Numero 1: <input type="number" name="num1"></p>
+    <p>Numero 2: <input type="number" name="num2"></p>
+    <p>Numero 3: <input type="number" name="num3"></p>
+    <p> &emsp; <input type="submit" value="Dividir"> <input type="reset" value="Limpiar"></p>
+    
+</form>

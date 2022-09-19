1a566b7743ecf92c78ba12b1906dec971ed58f54
gerardoJoseVilledaErazo <gerardovilleda@gmail.com>
2022-09-18 19:58:28 -0600

    Se creo el archivo restaa.php en la carpeta paginas

---
+++ application/views/paginas/resta.php
@@ -0,0 +1,6 @@
+<form action="../restar" method="POST">
+    <p>Numero 1: <input type="number" name="num1"></p>
+    <p>Numero 2: <input type="number" name="num2"></p>
+    <p> &emsp; <input type="submit" value="Restar"> <input type="reset" value="Limpiar"></p>
+    
+</form>

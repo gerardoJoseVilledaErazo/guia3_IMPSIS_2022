1f6d6c04406577b30570d1a976873e0b92c09859
gerardoJoseVilledaErazo <gerardovilleda@gmail.com>
2022-09-18 19:59:17 -0600

    Se creo el archivo multiplicacion.php en la carpeta paginas

---
+++ application/views/paginas/multiplicacion.php
@@ -0,0 +1,9 @@
+<form action="../multiplicar" method="POST">
+    <p>Numero 1: <input type="number" name="num1"></p>
+    <p>Numero 2: <input type="number" name="num2"></p>
+    <p>Numero 3: <input type="number" name="num3"></p>
+    <p>Numero 4: <input type="number" name="num4"></p>
+    <p>Numero 5: <input type="number" name="num5"></p>
+    <p> &emsp; <input type="submit" value="Multiplicar"> <input type="reset" value="Limpiar"></p>
+    
+</form>
